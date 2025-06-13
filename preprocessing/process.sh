877939f22507eb631374f777379652a60392a378
yibozhong <yibozhong657@gmail.com>
2025-06-13 23:48:23 +0800

    multi-gpu preprocess

--- preprocessing/process.sh
+++ preprocessing/process.sh
@@ -6,3 +6,12 @@ python dataset_tools.py convert --source=[YOUR_DOWNLOAD_PATH]/ILSVRC/Data/CLS-LO
 # Convert the pixel data to VAE latents
 python dataset_tools.py encode --source=${TARGET_PATH}/images \
     --dest=${TARGET_PATH}/vae-sd
+
+
+torchrun --nproc_per_node=8 preprocess.py \
+    --source=[YOUR_DOWNLOAD_PATH]/ILSVRC/Data/CLS-LOC/train \
+    --dest=${TARGET_PATH}/vae-sd \
+    --dest-images=${TARGET_PATH}/images \
+    --batch-size=128 \
+    --resolution=256 \
+    --transform=center-crop-dhariwal
