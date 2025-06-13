83c2640c1994df3a2bc3d7efef8f88c25d9ad9e9
yibozhong <yibozhong657@gmail.com>
2025-06-14 01:31:44 +0800

    update preprocess script

--- preprocessing/process.sh
+++ preprocessing/process.sh
@@ -1,12 +1,4 @@
 TARGET_PATH="../data/imagenet"
-# Convert raw ImageNet data to a ZIP archive at 256x256 resolution
-python dataset_tools.py convert --source=[YOUR_DOWNLOAD_PATH]/ILSVRC/Data/CLS-LOC/train \
-    --dest=${TARGET_PATH}/images --resolution=512x512 --transform=center-crop-dhariwal
-
-# Convert the pixel data to VAE latents
-python dataset_tools.py encode --source=${TARGET_PATH}/images \
-    --dest=${TARGET_PATH}/vae-sd
-
 
 torchrun --nproc_per_node=8 preprocess.py \
     --source=[YOUR_DOWNLOAD_PATH]/ILSVRC/Data/CLS-LOC/train \
